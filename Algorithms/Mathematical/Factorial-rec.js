35b6810b16e3d038e301346b4c1a50d067ab0eb8
Theofanis Despoudis <thdespou@hotmail.com>
2014-06-30 14:54:51 +0100

    +Factorial,Sum

---
+++ Algorithms/Mathematical/Factorial-rec.js
@@ -0,0 +1,29 @@
+/**
+*  Computer science for JavaScript
+*  Copyright 2014 Theofanis Despoudis
+*/
+
+/**
+* Computes the factorial number of n using recursion.
+* 
+* @module Algorithms
+* @submodule Mathematical
+* @param {Number} n A positive number
+* @return {Number} Factorial n!
+*
+* @example
+*   Factorial(3);
+*   ==>6
+*   Factorial(7);
+*   ==>5040
+* 
+*/
+function Factorial(n) {
+    //Compute n!
+    if (n <= 1) {
+        return 1;
+    }
+    else {
+        return n * Factorial(n - 1);
+    }
+}
---
+++ Algorithms/Mathematical/Sum.js
@@ -0,0 +1,29 @@
+/**
+*  Computer science for JavaScript
+*  Copyright 2014 Theofanis Despoudis
+*/
+
+/**
+* Computes the sum of numbers in array a[0:n-1]
+* 
+* @module Algorithms
+* @submodule Mathematical
+* @param {Array} a An array
+* @return {Number} The sum of numbers in array a
+*
+* @example
+*   Sum([1,66,-9,57,34]);
+*   ==>149
+*   Sum([1,2,3,4,5,6,7,8,9]);
+*   ==>45
+* 
+*/
+
+function Sum(array) {
+    // Computes array[0] + array[1] + ... + array[n-1]
+    var tsum = 0;
+    for(var i = 0, l = array.length;i <l ;i+=1) {
+        tsum +=array[i];
+    }
+    return tsum;
+}
