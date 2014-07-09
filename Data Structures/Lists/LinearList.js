56647d1123afd25f22e9c39ddd9003d312540f4a
Theofanis Despoudis <thdespou@hotmail.com>
2014-07-09 21:48:05 +0100

    Added More Methods for ArrayList

--- Data Structures/Lists/LinearList.js
+++ Data Structures/Lists/LinearList.js
@@ -1,4 +1,4 @@
-/**
+ /**
 *  Computer science for JavaScript
 *  Copyright 2014 Theofanis Despoudis
 */
@@ -28,8 +28,8 @@ function LinearList(MaxSize) {
 * @private
 */
     this._length = 0;
-    
- /**
+
+    /**
 * The private dynamic array
 * @property _arr
 * @type array
@@ -47,7 +47,13 @@ LinearList.prototype = {
 
     //constructor
     constructor: LinearList,
-    
+
+    //destructor called explicitely
+    Destroy: function() {
+        this._arr = [];
+        this._length = 0;
+    },
+
     /**
      * Checks if ArrayList is empty
      * @return {Boolean}
@@ -56,7 +62,7 @@ LinearList.prototype = {
     IsEmpty: function() {
         return this._length === 0;
     },
-    
+
     /**
      * Returns the current length of the ArrayList
      * @return {int}
@@ -65,20 +71,20 @@ LinearList.prototype = {
     Length: function() {
         return this._length;
     },
-    
+
     /**
      * Return the n'th element of the ArrayList or false otherwise
      * @param {int} n 
-     * @return {Object} The n'th element if exists
+     * @return {variant} The n'th element if exists or false otherwise
      * @method Find
      */
     Find: function(n) {
         //check bounds
-        if (n < 0 || n > this._length) {
+        if (n < 1 || n > this._length) {
             return false;
         }
         // it exists so return it
-        return this._arr[n-1];
+        return this._arr[n - 1];
     },
     /**
      * Try to locate the i position of element  if found
@@ -87,16 +93,46 @@ LinearList.prototype = {
      * @method Find
      */
     Search: function(elem) {
-        var i,l=this._length;
-        for(i=0;i<l;i+=1) {
+        var i, l = this._length;
+        for (i = 0; i < l; i += 1) {
             if (elem === this._arr[i]) {
                 return ++i;
             }
         }
         // not found
         return 0;
-    }
-    //Delete,
+    },
+
+    /**
+     * Delete the element elem on the n'th position and set it to elem
+     * @param {Object} elem The element that was deleted
+     * @param {int} n The position of the deleted element
+     * @return {ArrayList} The modified ArrayList
+     * @method Delete
+     */
+    Delete: function(elem, n) {
+        
+        var i, n = n || 1;
+        //check bounds
+        if (n < 1 || n > this._length) {
+            return this;
+        }
+
+        //check if empty
+        if (this.IsEmpty()) {
+            return this;
+        }
+        
+        elem = this.Find(n);
+        if (elem) {
+            //shift elements on the left of n
+            for (i = n; i < this._length; i += 1) {
+                this._arr[i - 1] = this._arr[i];
+            }
+        }
+        this._length -= 1;
+        return this;
+    },
     /**
      * Insert the element elem after the n'th position
      * @param {Object} elem The element to be inserted
@@ -104,10 +140,77 @@ LinearList.prototype = {
      * @return {ArrayList} The modified ArrayList
      * @method Insert
      */
-    //Insert: function(elem,n) {}
-    //ToString,
-    //Alternate,
-    //Split,
-    //Reverse,
-    //Merge
-}
+    Insert: function(elem, n) {
+        
+        var i, n = n || 0;
+        //check bounds
+        if (n < 0 || n > this._length) {
+            return this;
+        }
+        //check if array is full
+        if (this._length > this._maxsize) {
+            return this;
+        }
+        
+        for (i = this._length - 1; i >= n; i -= 1) {
+            //shift elements on the right of n
+            this._arr[i + 1] = this._arr[i];
+        }
+        this._arr[n] = elem;
+        this._length += 1;
+        return this;
+    },
+    /**
+     * Returns the String representation of the ArrayList
+     * @return {String} A string representation of the ArrayList.
+     * @method toString
+     */
+    toString: function() {
+        var i, l = this._length,result="";
+        for (i = 0; i < l; i += 1) {
+            result+=this._arr[i].toString();
+        }
+        return result;
+    },
+//Alternate,
+
+    /**
+     * Splits the LinearList into 2 LinearLists A and B that each of them will contain
+     * the even and the odd elements respectively
+     * @param {LinearList} A The first LinearList to be populated
+     * @param {LinearList} B The second LinearList to be populated
+     * @return {Boolean} True if successful
+     * @method Split
+     */
+    Split: function(A, B) {
+
+        //make sure the lengths are correct A.length + B.length = this._length
+        var len1 = Math.ceil(this._length / 2);
+        var len2 = this._length - len1;
+        
+        //make sure A and B are LinearLists
+        var B = B || new LinearList(len2);
+        var A = A || new LinearList(len1);
+        
+        if (A.Length() > A._maxsize || B.Length() > B._maxsize) {
+            //not enough space for result
+            return false;
+        }
+
+        //now we store the ith in A and ith+1 in B while either runs out of space
+        var i = 0, j = 0; // index for current array and A,B arrays
+        while (i <= B._maxsize) {
+            A._arr[j] = this._arr[i++];
+            B._arr[j++] = this._arr[i++];
+        }
+
+        // is an element left?
+        if (A._maxsize > B._maxsize) {
+            A._arr[j] = this._arr[i];
+        }
+        return true;
+    }
+//Reverse,
+//Merge
+};
+
